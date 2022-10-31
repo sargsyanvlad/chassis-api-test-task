752f11bd74e8bc3fcdc659fca35636d7c92627bd
volodya.sargsyan <volodya.sargsyan@burberry.com>
2022-10-31 15:59:22 +0400

    feat: test task implemented

---
+++ src/question/dto/update-question.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty } from 'class-validator';
+
+export class UpdateQuestionDto {
+	@ApiProperty()
+	@IsBoolean()
+	isPublic: boolean;
+
+	@ApiProperty()
+	@IsNotEmpty()
+	section: [
+		{
+			description: string;
+			questions: [string];
+		},
+	];
+}
